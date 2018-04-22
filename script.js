aac7926d01900ea74d0609e8e946a3488e69a81d
unknown <michaelcordero88@gmail.com>
2018-04-21 20:22:00 -0700

    added axis

--- script.js
+++ script.js
@@ -1,43 +1,62 @@
 let dataset=[20,45,78,100,15];
-let width= 600;
-let height= 600;
-let padding= 20;
+let dataset2=[[20,45],[30,70],[100,50],[150,30],[70,35],[280,180],[15,30],[20,95]]
+
+let padding= 30;
+let width= 600+ padding;
+let height= 600+ padding;
 let chart= d3.select("#chart")
 .append("svg")
 .attrs({
     "width": width,
     "height": height
 });
+let xscale= d3.scaleLinear();
+xscale.domain([0,d3.max(dataset, function(d){
+    return d;
+})]);
+xscale.range([0,height-(padding*2)]);
+
+let yscale= d3.scaleLinear();
+
+yscale.domain([0,d3.max(dataset, function(d){
+    return d;
+})]);
+yscale.range([height-padding,padding]);
 
 ///d3 3 phase enter update remove
+let xAxis=d3.axisBottom(xscale).tickSize([0]).ticks(0);
+let yAxis=d3.axisLeft(yscale);
 
 chart.selectAll("rect")
 .data(dataset)
 .enter()
 .append("rect")
 .attrs({
- "width": 50,
+ "width": function(){
+     return (width/dataset.length)-(padding *2);
+ },
  "height": function(d){
-     return d;
+     return xscale(d);
  },
  "x": function(d,i){
-     return (50+padding)*i;
+     return (width/dataset.length)*i+ padding;
  },
  "y": function(d){
-     return height-d;
+     return (height-xscale(d))- padding;
  },
  "fill": "teal",
 
 });
 
 chart .selectAll("rect")
-.on("mouseenter",function(){
+.on("mouseenter",function(d){
+    console.log(d);
     let x= d3.mouse(this)[0];
     let y= d3.mouse(this)[1];
     if(!document.getElementById("tooltip")){
-    console.log('added');
+  
     chart.append("text")
-   .text("text")
+   .text(d)
    .attrs({
        "id": "tooltip",
        "x": x,
@@ -50,7 +69,7 @@ chart .selectAll("rect")
   let x= d3.mouse(this)[0];
   let y= d3.mouse(this)[1];
 
-  console.log(d3.mouse(this));
+ 
   let tooltip= document.getElementById("tooltip");
   
  
@@ -67,7 +86,14 @@ chart .selectAll("rect")
       d3.select("#tooltip").remove();  
     }
    
-})
+});
+chart.append("g")
+.attr("class","yaxis")
+.attr("transform","translate("+padding+",0)")
+.call(yAxis);
 
-;
+chart.append("g")
+.attr("class","xaxis")
+.attr("transform","translate("+padding+","+(height-padding)+")")
+.call(xAxis);
 
